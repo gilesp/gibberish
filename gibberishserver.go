2acc118861d229b5c3094201ee687ce4705e26c7
gilesp <giles@vurt.co.uk>
2013-06-19 15:57:16 +0100

    Added facility to read file line by line - handy for parsing twitter archive dumps

--- gibberishserver.go
+++ gibberishserver.go
@@ -1,21 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/gilesp/markov"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
 func main() {
-	filename, port, root, splitterFlag, order := parseFlags()
+	filename, port, root, splitterFlag, order, lines := parseFlags()
 	fmt.Printf("Generating chain of order %d using file(s) \"%s\"...\n", order, filename)
 
 	chain := markov.NewChainWithSplitter(order, makeSplitter(splitterFlag))
-	populateChain(*chain, strings.Split(filename, ","))
+	populateChain(*chain, strings.Split(filename, ","), lines)
 	chainHandler := &ChainHandler{*chain}
 
 	http.HandleFunc("/gibberish", chainHandler.handle)
@@ -27,12 +29,13 @@ func main() {
 	}
 }
 
-func parseFlags() (filename string, port string, root string, splitterFlag string, order int) {
+func parseFlags() (filename string, port string, root string, splitterFlag string, order int, lines bool) {
 	flag.StringVar(&filename, "file", "corpus.txt", "Filename containing source text to imitate.")
 	flag.StringVar(&port, "port", "7070", "Port number to listen on.")
 	flag.StringVar(&root, "root", "public_html", "Folder to serve")
-	flag.StringVar(&splitterFlag, "splitter", "naive", "Sentence splitter to use. Currently accepts \"naive\" or \"illinois\"")
+	flag.StringVar(&splitterFlag, "splitter", "naive", "Sentence splitter to use. Currently accepts \"naive\", \"illinois\" or \"dub\"")
 	flag.IntVar(&order, "order", 4, "Length of prefix to use.")
+	flag.BoolVar(&lines, "lines", false, "Read input files line by line, rather than all in one go")
 	flag.Parse()
 	return
 }
@@ -43,6 +46,9 @@ func makeSplitter(splitterName string) markov.Splitter {
 	case "illinois":
 		fmt.Println("Using illinois sentence splitter")
 		splitter = markov.NewIllinoisSplitter()
+	case "dub":
+		fmt.Println("Swallowing the feather")
+		splitter = markov.NewDubSplitter()
 	case "naive":
 		fallthrough
 	default:
@@ -63,12 +69,38 @@ func loadTextFromFile(filename string) string {
 	}
 }
 
-func populateChain(chain markov.Chain, filenames []string) {
+func loadLinesFromFile(filename string) []string {
+	lines := []string{}
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error opening file: %v", err)
+		return lines
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err = scanner.Err(); err != nil {
+		fmt.Println("Error reading file: %v", err)
+	}
+	return lines
+}
+
+func populateChain(chain markov.Chain, filenames []string, lines bool) {
 	for _, filename := range filenames {
 		fmt.Printf("Parsing %s...\n", filename)
-		text := loadTextFromFile(filename)
-		if text != "" {
-			chain.Populate(text)
+		if lines {
+			lines := loadLinesFromFile(filename)
+			if len(lines) > 0 {
+				for _, line := range lines {
+					chain.Populate(line)
+				}
+			}
+		} else {
+			text := loadTextFromFile(filename)
+			if text != "" {
+				chain.Populate(text)
+			}
 		}
 	}
 }
